46f498d237dd7752a31fd858f8ad88cff998d30c
Soham Malakar <sohammalakarofficial@gmail.com>
2023-11-13 12:40:46 +0530

    Landing Page, and search component complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LandingPage from "./screens/LandingPage.jsx";
+import Header from './components/Header.jsx';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
--- client/src/screens/LandingPage.jsx
+++ client/src/screens/LandingPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-
+import Search from "../components/Search";
 const LandingPage = () =>{
     return(
-        <div className=" bg-gradient-to-tr from-gray-200 to-gray-300 h-screen w-screen flex p-20" >
-            <div className="w-1/2 h-fit flex flex-col">
-                <h1 className="text-8xl font-bold track-wider text-blue-950 mb-4">Find Your Dream <span className=" text-green-900 h-fit w-fit">Job</span></h1>
-                <p className="text-2xl font-semibold text-gray-600">Get yourself exposed to thousands of jobs at our platform, suited to your qualifications. New job postings everyday!</p>
+        <div className=" bg-slate-200 h-fit w-screen flex flex-col px-8 md:flex-row pb-8" >
+            <div className="w-full md:w-1/2 h-fit flex flex-col pt-10">
+                <h1 className="text-6xl md:text-8xl font-bold track-wider mb-4">Find Your Dream <span className=" text-red-400 h-fit w-fit">Job</span></h1>
+                <p className="text-xl md:text-2xl font-semibold text-gray-700">Get yourself exposed to thousands of jobs at our platform, suited to your qualifications. New job postings everyday!</p>
+                <div className=" hidden md:block">
+                    <Search />
+                </div>
             </div>
-            <div className="w-1/2">
-                <img src="/assets/jobseeker.png" alt="seeker.img" className=" h-4/5"/>
+            <div className="w-full md:w-1/2 flex flex-col">
+                <img src="/assets/jobseeker.png" alt="seeker.img" className=" h-full w-full mx-auto"/>
+                <div className=" block md:hidden">
+                    <Search />
+                </div>
             </div>
         </div>
     )
